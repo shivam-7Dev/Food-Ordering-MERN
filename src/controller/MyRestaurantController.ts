f27493884f6b3694f2d7964543a745400ead2fb7
Shivam Sharma <shivam7.dev@gmail.com>
2024-05-25 17:41:26 +0530

    added the update restaurant api

--- src/controller/MyRestaurantController.ts
+++ src/controller/MyRestaurantController.ts
@@ -3,6 +3,15 @@ import { Request, Response } from "express";
 import cloudinary from "cloudinary";
 import mongoose from "mongoose";
 
+const uploadImage = async (file: Express.Multer.File) => {
+  const image = file;
+  const base64Image = Buffer.from(image.buffer).toString("base64");
+  const dataURI = `data:${image.mimetype};base64,${base64Image}`;
+
+  const uploadResponse = await cloudinary.v2.uploader.upload(dataURI);
+  return uploadResponse.url;
+};
+
 const createMyRestaurant = async (req: Request, res: Response) => {
   try {
     console.log("-----------");
@@ -60,4 +69,36 @@ const getMyRestaurant = async (req: Request, res: Response) => {
   }
 };
 
-export default { createMyRestaurant, getMyRestaurant };
+const updateMyRestaurant = async (req: Request, res: Response) => {
+  try {
+    // find the restaurant you want to update
+    const restaurant = await Restaurant.findOne({
+      user: req.userId,
+    });
+
+    if (!restaurant) {
+      return res.status(404).json({ message: "restaurant not found" });
+    }
+    // update all the individual fields of the restaurant
+    restaurant.restaurantName = req.body.restaurantName;
+    restaurant.city = req.body.city;
+    restaurant.country = req.body.country;
+    restaurant.deliveryPrice = req.body.deliveryPrice;
+    restaurant.estimatedDeliveryTime = req.body.estimatedDeliveryTime;
+    restaurant.cuisines = req.body.cuisines;
+    restaurant.menuItems = req.body.menuItems;
+
+    if (req.file) {
+      const imageUrl = await uploadImage(req.file as Express.Multer.File);
+      restaurant.imageUrl = imageUrl;
+    }
+
+    await restaurant.save();
+    res.status(200).send(restaurant);
+  } catch (error) {
+    console.log("error", error);
+    res.status(500).json({ message: "Something went wrong" });
+  }
+};
+
+export default { createMyRestaurant, getMyRestaurant, updateMyRestaurant };
